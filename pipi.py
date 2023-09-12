0b2866c98d9ca005ed59c5755fb1bd8c80d24006
M0kuj1nn <mibnight111@gmail.com>
2023-09-12 21:58:43 +0700

    replaced classes with a named typle. Wrapped all loops in functions and rewrote thr code for PEP8

--- pipi.py
+++ pipi.py
@@ -1,23 +1,24 @@
+from collections import namedtuple
 with open('pupu.txt', 'r') as file:
     lines = file.readlines()
 
-class EducationClass:
-    def __init__(self, type, data, auditoria, teacher):
-        self.type = type
-        self.data = data
-        self.auditoria = auditoria
-        self.teacher = teacher
+Schedule = namedtuple('Schedule', 'type data classroom teacher')
 
-    def pr(self):
-        print(type, self.type, self.data, self.auditoria, self.teacher)
-
-mylist = []
-for string in lines:
-    buff = string.split()
-    day = EducationClass(buff[0], buff[1], buff[2], buff[3])
-    mylist.append(day)
+day_list = []
 
 
-for t in mylist:
-    print(t.type, t.data, t.auditoria, t.teacher)
+def list_of_objects(obj, lines):
+    for string in lines:
+        buff = string.split()
+        day = obj(buff[0], buff[1], buff[2], buff[3])
+        day_list.append(day)
 
+
+def print_entire_schedule():
+    for elem in day_list:
+        print(elem.type, elem.data, elem.classroom, elem.teacher)
+
+
+if __name__ == '__main__':
+    list_of_objects(Schedule, lines)
+    print_entire_schedule()
