bba340abc69b2bd212e9e6c615a9c4509799e8e9
M0kuj1nn <mibnight111@gmail.com>
2023-09-16 20:46:29 +0700

    Third commit

--- pipi.py
+++ pipi.py
@@ -2,23 +2,32 @@ from collections import namedtuple
 with open('pupu.txt', 'r') as file:
     lines = file.readlines()
 
-Schedule = namedtuple('Schedule', 'type data classroom teacher')
 
-day_list = []
+class Date:
+    def __init__(self, date):
+        self.date = date
 
 
-def list_of_objects(obj, lines):
+class EducationClass:
+    def __init__(self, classroom, teacher, date: Date):
+        self.classroom = classroom
+        self.teacher = teacher
+        self.date = date
+
+
+def list_of_objects(obj, lines) -> list:
+    day_list = []
     for string in lines:
         buff = string.split()
-        day = obj(buff[0], buff[1], buff[2], buff[3])
-        day_list.append(day)
+        ed_class = obj(buff[0], buff[1], buff[2])
+        day_list.append(ed_class)
+    return day_list
 
 
-def print_entire_schedule():
-    for elem in day_list:
-        print(elem.type, elem.data, elem.classroom, elem.teacher)
+def print_entire_schedule(list):
+    for elem in list:
+        print(elem.classroom, elem.teacher, elem.date)
 
 
 if __name__ == '__main__':
-    list_of_objects(Schedule, lines)
-    print_entire_schedule()
+    print_entire_schedule(list_of_objects(EducationClass, lines))
